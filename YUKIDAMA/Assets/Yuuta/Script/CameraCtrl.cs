e7b3af146dc48b71940ca1782542861ee01373b1
yuuta0812 <65881461+yuuta0812@users.noreply.github.com>
2021-01-14 14:07:50 +0900

    Player01140207

---
+++ YUKIDAMA/Assets/Yuuta/Script/CameraCtrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour
+{
+    Transform target;
+    [SerializeField]
+    Vector3 offset;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        transform.position = target.position + offset;
+    }
+}
